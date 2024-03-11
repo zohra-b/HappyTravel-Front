1c9a5cb561f8210b5c82ad9d175c5e4d0e6307db
annaLucian <garielapoves01@gmail.com>
2024-03-11 15:53:39 +0100

    create new pagination scroll infinite

--- src/components/Cardlist.jsx
+++ src/components/Cardlist.jsx
@@ -1,25 +1,44 @@
 "use client";
 import { useEffect, useState } from "react";
-import { getTripsByPage } from "@/services/";
+import { getTripsByPage, getAllTrips } from "@/services/";
 import Card from "@/components/Card.jsx";
 import Pagination from "@/components/Pagination";
+import useBreakpoint from "use-breakpoint";
+
+const BREAKPOINTS = { mobile: 0, desktop: 1000 };
 
 export default function CardList() {
   const [trips, setTrips] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const { breakpoint } = useBreakpoint(BREAKPOINTS);
 
   useEffect(() => {
     const fetchTrips = async () => {
       try {
         const tripsData = await getTripsByPage(currentPage);
+        setTrips(tripsData.data);
+        setTotalPages(Math.ceil(tripsData.total / 8));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (breakpoint === "desktop") fetchTrips();
+  }, [currentPage, breakpoint]);
+
+  useEffect(() => {
+    const fetchTrips = async () => {
+      try {
+        const tripsData = await getAllTrips();
         setTrips(tripsData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
 
-    fetchTrips();
-  }, [currentPage]);
+    if (breakpoint === "mobile") fetchTrips();
+  }, [breakpoint]);
 
   const handlePagination = (newPage) => {
     setCurrentPage(newPage);
@@ -28,15 +47,18 @@ export default function CardList() {
   return (
     <>
       <section className="lg:flex lg:flex-wrap lg:gap-[1rem] lg:max-w-[80%] lg:mx-auto lg:mt-[2rem]">
-        {trips.map((trip) => (
-          <Card key={trip.id} trip={trip} />
+        {trips?.map((trip) => (
+          <Card key={trip.id} trip={trip} totalPages={totalPages} />
         ))}
       </section>
       <section className="lg:flex lg:max-w-[80%] lg:mx-auto items-center justify-center lg:my-[2rem]">
-        <Pagination
-          handlePagination={handlePagination}
-          currentPage={currentPage}
-        />
+        {breakpoint === "desktop" && (
+          <Pagination
+            handlePagination={handlePagination}
+            currentPage={currentPage}
+            totalPages={totalPages}
+          />
+        )}
       </section>
     </>
   );
--- src/services/index.jsx
+++ src/services/index.jsx
@@ -3,9 +3,11 @@ axios.defaults.headers.post["Content-Type"] = "application/json";
 axios.defaults.headers.post["Accept"] = "application/json";
 
 const API_URL = process.env.API_URL;
+
 export const getAllTrips = async () => {
   try {
     const response = await axios.get(API_URL);
+    console.log(response);
     return response.data;
   } catch (error) {
     console.log("Error: ", error);
@@ -14,7 +16,7 @@ export const getAllTrips = async () => {
 export const getTripsByPage = async (currentPage) => {
   try {
     const response = await axios.get(`${API_URL}/page?page=${currentPage}`);
-    return response.data.data;
+    return { data: [...response.data.data], total: response.data.total };
   } catch (error) {
     console.log("Error: ", error);
   }

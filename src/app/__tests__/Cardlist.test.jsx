1c1c197419a487c30eddf74cb53e53b1b8cbc5f6
annaLucian <garielapoves01@gmail.com>
2024-03-11 12:33:09 +0100

    change rename file

--- src/app/__tests__/Cardlist.test.jsx
+++ src/app/__tests__/Cardlist.test.jsx
@@ -1,32 +1,32 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import CardList from '../../components/Cardlist.jsx'; 
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import CardList from "../../components/CardList.jsx";
 
-jest.mock('../../services/index.jsx', () => ({
-  getTripsByPage: jest.fn().mockResolvedValue([{ id: 1, title: 'Trip 1', location: 'Location 1' }])
+jest.mock("../../services/index.jsx", () => ({
+  getTripsByPage: jest
+    .fn()
+    .mockResolvedValue([{ id: 1, title: "Trip 1", location: "Location 1" }]),
 }));
 
-describe('Cardlist Component', () => {
-  test('renderiza correctamente y muestra la lista de viajes', async () => {
+describe("Cardlist Component", () => {
+  test("renderiza correctamente y muestra la lista de viajes", async () => {
     render(<CardList />);
-    await screen.findByText('Trip 1');
-    expect(screen.getByText('Trip 1')).toBeInTheDocument();
-    expect(screen.getByText('Location 1')).toBeInTheDocument();
+    await screen.findByText("Trip 1");
+    expect(screen.getByText("Trip 1")).toBeInTheDocument();
+    expect(screen.getByText("Location 1")).toBeInTheDocument();
   });
 
-  test('cambia de página al hacer clic en la paginación', async () => {
+  test("cambia de página al hacer clic en la paginación", async () => {
     render(<CardList />);
-    await screen.findByText('Trip 1');
-    
-    
-    userEvent.click(screen.getAllByRole('button')[1]);
-    
-    await screen.findByText('Trip 1');
-    expect(screen.getByText('Trip 1')).toBeInTheDocument();
-    
-    
-    await screen.findByText('Location 1');
-    expect(screen.queryByText('Location 2')).not.toBeInTheDocument();
+    await screen.findByText("Trip 1");
+
+    userEvent.click(screen.getAllByRole("button")[1]);
+
+    await screen.findByText("Trip 1");
+    expect(screen.getByText("Trip 1")).toBeInTheDocument();
+
+    await screen.findByText("Location 1");
+    expect(screen.queryByText("Location 2")).not.toBeInTheDocument();
   });
 });

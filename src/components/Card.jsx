5db6015a58fab5f0b677b34ba2ba9651a5563083
annaLucian <garielapoves01@gmail.com>
2024-03-13 17:26:54 +0100

    create component squeleton and the functionality of the search engine has been fixed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ trip }) {
   return (
-    <div className="bg-primary-yellow w-[70%] mx-auto rounded-lg mb-[1rem] lg:basis-[21%]">
+    <div className="bg-primary-yellow w-[70%] rounded-lg mb-[1rem] lg:basis-[21rem]">
       <div className="w-full h-[15rem] bg-[url('/image/IslaEjemplo.jpg')] bg-no-repeat bg-center bg-cover rounded-lg"></div>
       <div className="pl-[0.8rem] pb-[1rem] ">
         <h1 className="text-quaternary-blue font-bold text-[1.1rem] mt-[0.5rem]">

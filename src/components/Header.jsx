1334efe250df330c45c201e9cf59e1c630942b81
annaLucian <garielapoves01@gmail.com>
2024-03-13 18:39:38 +0100

    the search engine component is only visible on the home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import Image from "next/image";
 import NavBar from "./NavBar";
 import Search from "./Search";
 
 export default function Header() {
+  const pathname = usePathname();
   return (
     <header className="mt-[2rem] flex flex-col items-center lg:max-w-[78%] lg:flex-row lg:mx-auto lg:border-b-[0.1rem] lg:border-quaternary-blue">
       <div className="flex flex-col w-full items-center gap-[1.5rem] lg:flex-row lg:basis-[90%] lg:justify-between ">
@@ -13,7 +17,7 @@ export default function Header() {
           height={100}
           className="w-[8rem] lg:pb-[0.5rem]"
         />
-        <Search />
+        {pathname === "/" && <Search />}
       </div>
       <NavBar />
     </header>

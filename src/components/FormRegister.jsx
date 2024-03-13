f095e3bf3e374f54a1712f94dd2b50c039c7c0a3
ImLauraGS <lgilsolano@gmail.com>
2024-03-13 18:52:15 +0100

    validations register taken from back done

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -4,8 +4,6 @@ import InputForm from './InputForm';
 import Btn from './Btn';
 import { registerUser } from "@/services/";
 
-
-
 export default function FormRegister() {
   const [formData, setFormData] = useState({
     name: '',
@@ -13,11 +11,15 @@ export default function FormRegister() {
     password: ''
   });
 
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
   const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
-    });
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: "" });
   };
 
   const handleSubmit = async (e) => {
@@ -25,47 +27,55 @@ export default function FormRegister() {
     try {
       const response = await registerUser(formData);
       console.log('Usuario registrado exitosamente: ', response);
-      alert('Usuario registrado exitosamente')
-   
+      alert('Usuario registrado exitosamente');
     } catch (error) {
-      console.error('Error al registrar usuario: ', error);
+      if (error.response && error.response.data.errors) {
+        const errorData = error.response.data.errors;
+        setErrors({
+          name: errorData.name ? errorData.name : "",
+          email: errorData.email ? errorData.email : "",
+          password: errorData.password ? errorData.password : ""
+        });
+      } else {
+        console.error('Error al registrar usuario: ', error);
+      }
     }
   };
 
- 
-
   return (
-    <div className="flex flex-col w-[370px] min-h-[487px] gap-6 rounded-2xl border-4 items-center border-primary-yellow pb-14">
-    <h2 className="text-xl text-tertiary-red font-bold pt-3">Registro de usuario</h2>
-    <form onSubmit={handleSubmit} className="border-t-2 border-tertiary-red flex flex-col">
-      <label htmlFor="name" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
-        Nombre
-      </label>
-      <InputForm type="text" placeholder="Escribe tu nombre..." name="name" value={formData.name} onChange={handleChange}/>
-      <label htmlFor="email" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
-        Email
-      </label>
-      <InputForm type="email" placeholder="Escribe tu correo electrónico..." name="email" value={formData.email} onChange={handleChange}/>
-      <label htmlFor="password" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
-        Contraseña
-      </label>
-      <InputForm type="password" placeholder="Escribe tu contraseña..." value={formData.password} name="password" onChange={handleChange}/>
-      <div className="flex gap-4 justify-center pt-8">
-      </div>
-      <div className='flex gap-4 my-3'>
-        <Btn
-            text="Aceptar"
-            color="bg-secondary-green"
-            onSubmit={handleSubmit}
-        />
-        <Btn
-            text="Cancelar"
-            color="bg-tertiary-red"
-        />
-      </div>
-      <span className="text-quaternary-blue text-center text-xl font-bold">¿Ya tienes cuenta? Accede <a href="/login" className="text-secondary-green">aquí</a></span>
-    
-    </form>
-  </div>
-  )
+    <div className="flex flex-col w-[370px] min-h-[487px] gap-6 rounded-2xl border-4 items-center mt-5 border-primary-yellow pb-14">
+      <h2 className="text-xl text-tertiary-red font-bold pt-3">Registro de usuario</h2>
+      <form onSubmit={handleSubmit} className="border-t-2 border-tertiary-red flex flex-col">
+        <label htmlFor="name" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
+          Nombre
+        </label>
+        <InputForm type="text" placeholder="Escribe tu nombre..." name="name" value={formData.name} onChange={handleChange}/>
+        {errors.name && <p className="text-tertiary-red font-semibold">{errors.name}</p>}
+        <label htmlFor="email" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
+          Email
+        </label>
+        <InputForm type="email" placeholder="Escribe tu correo electrónico..." name="email" value={formData.email} onChange={handleChange}/>
+        {errors.email && <p className="text-tertiary-red font-semibold">{errors.email}</p>}
+        <label htmlFor="password" className="text-quaternary-blue text-xl font-bold pb-1 pt-6">
+          Contraseña
+        </label>
+        <InputForm type="password" placeholder="Escribe tu contraseña..." value={formData.password} name="password" onChange={handleChange}/>
+        {errors.password && <p className="text-tertiary-red font-semibold">{errors.password}</p>}
+        <div className="flex gap-4 justify-center pt-8">
+        </div>
+        <div className='flex gap-4 my-3'>
+          <Btn
+              text="Aceptar"
+              color="bg-secondary-green"
+              onSubmit={handleSubmit}
+          />
+          <Btn
+              text="Cancelar"
+              color="bg-tertiary-red"
+          />
+        </div>
+        <span className="text-quaternary-blue text-center text-xl font-bold">¿Ya tienes cuenta? Accede <a href="/login" className="text-secondary-green">aquí</a></span>
+      </form>
+    </div>
+  );
 }

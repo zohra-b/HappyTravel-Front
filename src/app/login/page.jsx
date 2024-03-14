29582a273ec81d70118b30f1bb74ef928267a003
AliciaoE <alialioh@gmail.com>
2024-03-14 11:10:48 +0100

    create logout

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,12 @@
 import FormLogin from '@/components/Login'
+import Logout from '@/components/Logout'
 
 export default function page() {
   return (
     <main className='flex items-center justify-center h-[70vh]'>
         <FormLogin/>
+
+  
     </main>
     
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,18 @@ import { usePathname } from "next/navigation";
 import Image from "next/image";
 import NavBar from "./NavBar";
 import Search from "./Search";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
   const pathname = usePathname();
+  const router = useRouter();
+
+  const handleLogout = () => {
+
+    localStorage.removeItem("token");
+
+    router.push("/login");
+  };
   return (
     <header className="mt-[2rem] flex flex-col items-center lg:max-w-[78%] lg:flex-row lg:mx-auto lg:border-b-[0.1rem] lg:border-quaternary-blue">
       <div className="flex flex-col w-full items-center gap-[1.5rem] lg:flex-row lg:basis-[90%] lg:justify-between ">
@@ -19,6 +28,7 @@ export default function Header() {
         />
         {pathname === "/" && <Search />}
       </div>
+      <button onClick={handleLogout}>Cerrar sesión</button>
       <NavBar />
     </header>
   );

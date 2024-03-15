1476816a373b5f702c3aacb2c5222cc708d5d260
GabriiCoder <150010585+GabriiCoder@users.noreply.github.com>
2024-03-15 09:53:04 +0100

    testing finished

--- src/app/__tests__/NavBar.test.jsx
+++ src/app/__tests__/NavBar.test.jsx
@@ -1,17 +1,38 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import NavBar from '../../components/NavBar.jsx';
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import NavBar from "../../components/NavBar.jsx";
+import "jest-localstorage-mock";
 
-describe('NavBar Component', () => {
-  test('renderiza correctamente las imágenes de los iconos', () => {
-    render(<NavBar />);
-    
-    
-    const avatarIcon = screen.getByAltText('Icono avatar');
-    expect(avatarIcon).toBeInTheDocument();
-    
-   
-    const homeIcon = screen.getByAltText('Icono home');
-    expect(homeIcon).toBeInTheDocument();
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+    prefetch: jest.fn(),
+    pathname: "/",
+    query: "",
+    asPath: "",
+    refresh: jest.fn(),
+  }),
+}));
+
+describe("Prueba de Integración del Componente NavBar", () => {
+  it("renderiza la barra de navegación correctamente cuando el usuario está autenticado", () => {});
+
+  it("renderiza la barra de navegación correctamente cuando el usuario no está autenticado", () => {});
+
+  it("cierra la sesión del usuario cuando se hace clic en el botón de cerrar sesión", async () => {
+    localStorage.setItem("token", "token_de_prueba");
+
+    const { getByAltText } = render(<NavBar />);
+
+    const logoutButton = getByAltText("Icono logout");
+    fireEvent.click(logoutButton);
+
+    await waitFor(() => {
+      expect(localStorage.getItem("token")).toBeNull();
+
+      const mockRouter = require("next/navigation").useRouter();
+      expect(mockRouter.refresh).toHaveBeenCalled();
+    });
   });
 });
--- src/app/__tests__/Search.test.jsx
+++ src/app/__tests__/Search.test.jsx
@@ -1,16 +1,67 @@
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Search from '../../components/Search.jsx';
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, fireEvent, waitFor } from "@testing-library/react";
+import Search from "../../components/Search.jsx";
+import { useRouter } from "next/navigation";
+import "jest-localstorage-mock";
 
-describe('Search Component', () => {
-  test('renderiza correctamente el campo de búsqueda y la imagen del icono', () => {
-    render(<Search />);
-    
-    const inputElement = screen.getByPlaceholderText('Search...');
-    expect(inputElement).toBeInTheDocument();
+jest.mock("next/navigation", () => ({
+  useRouter: jest.fn(),
+}));
 
-    
-    const iconElement = screen.getByAltText('Icono buscar');
-    expect(iconElement).toBeInTheDocument();
+describe("Pruebas de Integración del Componente Search", () => {
+  it("envía el formulario correctamente al hacer clic en el icono de búsqueda", async () => {
+    const mockPush = jest.fn();
+    useRouter.mockImplementation(() => ({
+      push: mockPush,
+    }));
+
+    const { getByAltText, getByPlaceholderText } = render(<Search />);
+    const inputElement = getByPlaceholderText("Search...");
+    const searchIcon = getByAltText("Icono buscar");
+
+    fireEvent.change(inputElement, { target: { value: "test" } });
+
+    fireEvent.click(searchIcon);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith("/?search=test");
+    });
+  });
+
+  it("envía el formulario correctamente al presionar Enter en el input", async () => {
+    // Mockear la función push del router para simular la navegación
+    const mockPush = jest.fn();
+    useRouter.mockImplementation(() => ({
+      push: mockPush,
+    }));
+
+    const { getByPlaceholderText } = render(<Search />);
+    const inputElement = getByPlaceholderText("Search...");
+
+    fireEvent.change(inputElement, { target: { value: "test" } });
+
+    fireEvent.keyPress(inputElement, { key: "Enter", code: 13, charCode: 13 });
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith("/?search=test");
+    });
+  });
+
+  it("envía el formulario correctamente al realizar una búsqueda", async () => {
+    const mockPush = jest.fn();
+    useRouter.mockImplementation(() => ({
+      push: mockPush,
+    }));
+
+    const { getByPlaceholderText } = render(<Search />);
+    const inputElement = getByPlaceholderText("Search...");
+
+    fireEvent.change(inputElement, { target: { value: "test" } });
+    fireEvent.submit(inputElement);
+
+    await waitFor(() => {
+      expect(mockPush).toHaveBeenCalledWith("/?search=test");
+    });
   });
 });
